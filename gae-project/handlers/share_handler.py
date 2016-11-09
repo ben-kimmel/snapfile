628bf14e49f0606a42d58b677c43d13e00b9eee8
ben-kimmel <kimmel.ben@gmail.com>
2016-11-09 13:06:18 -0500

    more fixes

--- gae-project/handlers/share_handler.py
+++ gae-project/handlers/share_handler.py
@@ -13,10 +13,11 @@ class ShareHandler(base_handlers.BaseAction):
         print(self.request.get("public"))
         for email_2 in self.request.get("emails").split(";"):
             print(email_2)
-            share = DirectShare(parent=utils.get_parent_key_for_email(email))
-            share.file = blobstore.get(self.request.get("file_key")).key()
-            share.available_from = dt.strptime(self.request.get("start"), "%Y-%m-%dT%H:%M")
-            share.available_until = dt.strptime(self.request.get("end"), "%Y-%m-%dT%H:%M")
-            share.email = email_2
-            share.put()
+            if self.request.get("file_key") and self.request.get("start") and self.request.get("end"):
+                share = DirectShare(parent=utils.get_parent_key_for_email(email))
+                share.file = blobstore.get(self.request.get("file_key")).key()
+                share.available_from = dt.strptime(self.request.get("start"), "%Y-%m-%dT%H:%M")
+                share.available_until = dt.strptime(self.request.get("end"), "%Y-%m-%dT%H:%M")
+                share.email = email_2
+                share.put()
         self.redirect("/files")
--- gae-project/static/js/main.js
+++ gae-project/static/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	$(document).on("click", ".share-modal-open", function() {
 		var fileKey = $(this).data('id');
 		$(".modal-body #share_file_key").val(fileKey);
+		$('.modal-body #share-link').attr("href", "/view/" + fileKey);
+		$(".modal-body #share-link").html("www.snapfile.biz/view/" + fileKey);
 	});
 	$(document).on("click", ".del-modal-open", function() {
 		var fileKey = $(this).data('id');
